f9e2d684ee5e2ed4cd0e1a0368db8ab13c8be720
HBO2 <bertvanhouten@icloud.com>
2014-06-26 22:47:34 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -199,6 +199,13 @@ for (int i = 0; i < [_gridArray count]; i++)
                 // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
                 if (!((x == i) && (y == j)) && isIndexValid)
                 {
+                    
+                    Creature *neighbor = _gridArray[x][y];
+                    if (neighbor.isAlive)
+                    {
+                        currentCreature.livingNeighbors += 1;
+                    }
+                    
                 
                     // check if the Creature's livingNeighbors property is set to 3
                     if (currentCreature.livingNeighbors == 3) {
