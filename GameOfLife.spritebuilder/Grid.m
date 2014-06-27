1bc6edf7d7cc4d91e1f81404b0a0fdc5d398d621
HBO2 <bertvanhouten@icloud.com>
2014-06-27 02:38:27 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -86,11 +86,7 @@ static const int GRID_COLUMNS = 10;
     //invert it's state - kill it if it's alive, bring it to life if it's dead.
     creature.isAlive = !creature.isAlive;
     
-    if (creature.isAlive) {
-        _totalAlive++;
-    } else {
-        _totalAlive--;
-    }
+  
     
     
 }
